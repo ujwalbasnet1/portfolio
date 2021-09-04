583dee8ed267b828ca5b8652eb1faac218ff4976
Ujwal Basnet <ujwalbasnet1@gmail.com>
2021-09-04 20:38:51 +0545

    changes.

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -2,58 +2,212 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio/helper/ui_helpers.dart';
 
-class HomePage extends StatelessWidget {
+const padding = const EdgeInsets.symmetric(
+  horizontal: 64,
+  vertical: 32,
+);
+
+class SectionModel {
+  final Widget section;
+  final String title;
+
+  SectionModel(this.title, this.section);
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late PageController _controller;
+  late int _currentIndex;
+
+  @override
+  void initState() {
+    _currentIndex = 0;
+    _controller = PageController(initialPage: _currentIndex);
+    super.initState();
+  }
+
+  List<SectionModel> get pages => [
+        SectionModel(
+          "Introduction",
+          Row(
+            children: [
+              Container(
+                width: screenWidth(context) * 0.35,
+                height: screenHeight(context, 0.9),
+                child: Center(
+                  child: Container(
+                    width: screenWidth(context, 0.5),
+                    child: _introSection(context),
+                  ),
+                ),
+              ),
+              Expanded(child: _imageSection(context)),
+            ],
+          ),
+        ),
+        SectionModel(
+          "Portfolios",
+          Row(
+            children: [
+              Expanded(child: _imageSection(context)),
+              Container(
+                width: screenWidth(context) * 0.35,
+                height: screenHeight(context, 0.9),
+                child: Center(
+                  child: Container(
+                    width: screenWidth(context, 0.5),
+                    child: _introSection(context),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SectionModel(
+          "Testimonials",
+          Row(
+            children: [
+              Container(
+                width: screenWidth(context) * 0.35,
+                height: screenHeight(context, 0.9),
+                child: Center(
+                  child: Container(
+                    width: screenWidth(context, 0.5),
+                    child: _introSection(context),
+                  ),
+                ),
+              ),
+              Expanded(child: _imageSection(context)),
+            ],
+          ),
+        ),
+        SectionModel(
+          "Contact",
+          Row(
+            children: [
+              Expanded(child: _imageSection(context)),
+              Container(
+                width: screenWidth(context) * 0.35,
+                height: screenHeight(context, 0.9),
+                child: Center(
+                  child: Container(
+                    width: screenWidth(context, 0.5),
+                    child: _introSection(context),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SectionModel(
+          "Location",
+          Row(
+            children: [
+              Container(
+                width: screenWidth(context) * 0.35,
+                height: screenHeight(context, 0.9),
+                child: Center(
+                  child: Container(
+                    width: screenWidth(context, 0.5),
+                    child: _introSection(context),
+                  ),
+                ),
+              ),
+              Expanded(child: _imageSection(context)),
+            ],
+          ),
+        ),
+      ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF0E0D37),
       appBar: _appBar(context),
       extendBodyBehindAppBar: true,
-      body: ListView(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 64,
-          vertical: 32,
-        ),
+      body: Stack(
         children: [
-          Row(
-            children: [
-              Container(
-                width: screenWidth(context) * 0.35,
-                height: screenHeight(context, 0.9),
-                child: Center(
-                  child: Container(
-                    width: screenWidth(context, 0.5),
-                    child: _introSection(context),
-                  ),
-                ),
+          Positioned.fill(
+            child: Padding(
+              padding: EdgeInsets.only(right: tocWidth),
+              child: PageView(
+                onPageChanged: (index) => setState(() => _currentIndex = index),
+                controller: _controller,
+                scrollDirection: Axis.vertical,
+                children: pages
+                    .map(
+                      (e) => Padding(
+                        padding: padding,
+                        child: e.section,
+                      ),
+                    )
+                    .toList(),
               ),
-              Expanded(child: _imageSection(context)),
-            ],
+            ),
           ),
-
-          SizedBox(height: 120),
-          Row(
-            children: [
-              Expanded(child: _imageSection(context)),
-              Container(
-                width: screenWidth(context) * 0.35,
-                height: screenHeight(context, 0.9),
-                child: Center(
-                  child: Container(
-                    width: screenWidth(context, 0.5),
-                    child: _introSection(context),
-                  ),
-                ),
-              ),
-            ],
+          Positioned(
+            right: 12,
+            bottom: 0,
+            top: 0,
+            child: _indicatorWidget(context),
           ),
-          // Row(
-          //   children: [
-          //     Expanded(child: _introSection(context)),
-          //     _imageSection(context),
-          //   ],
-          // ),
         ],
+      ),
+    );
+  }
+
+  double tocWidth = 44;
+
+  Widget _indicatorWidget(BuildContext context) {
+    return Center(
+      child: Container(
+        width: tocWidth,
+        child: ListView.separated(
+          padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 12),
+          physics: NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: pages.length,
+          itemBuilder: (context, index) {
+            String str = (index + 1).toString();
+            if ((index + 1) <= 9) str = "0" + str;
+
+            return InkWell(
+              onTap: (_currentIndex == index)
+                  ? null
+                  : () {
+                      _controller.animateToPage(
+                        index,
+                        duration: Duration(
+                            milliseconds: (_currentIndex - index).abs() * 300),
+                        curve: Curves.easeIn,
+                      );
+                    },
+              borderRadius: BorderRadius.circular(32),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 4.0,
+                  vertical: 8,
+                ),
+                child: Text(
+                  str,
+                  style: Theme.of(context).textTheme.subtitle2?.copyWith(
+                        fontWeight: FontWeight.bold,
+                        color: (_currentIndex == index)
+                            ? Colors.white
+                            : Colors.white54,
+                        fontSize: (_currentIndex == index) ? 24 : null,
+                      ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (context, index) => Divider(height: 0),
+        ),
       ),
     );
   }
@@ -93,9 +247,11 @@ class HomePage extends StatelessWidget {
       );
 
   Widget _introSection(BuildContext context) => Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
+        child: ListView(
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          // mainAxisSize: MainAxisSize.min,
+          physics: NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
           children: [
             Text(
               "Hello, I am",
@@ -155,25 +311,28 @@ class HomePage extends StatelessWidget {
               ],
             ),
             SizedBox(height: 32),
-            InkWell(
-              onTap: () {
-                // TODO
-              },
-              borderRadius: BorderRadius.circular(32),
-              child: Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 24,
-                  vertical: 8,
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(32),
-                ),
-                child: Text(
-                  "Hire Me",
-                  style: Theme.of(context).textTheme.subtitle1?.copyWith(
-                        color: Colors.white,
-                      ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: InkWell(
+                onTap: () {
+                  // TODO
+                },
+                borderRadius: BorderRadius.circular(32),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 8,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                  child: Text(
+                    "Hire Me",
+                    style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                          color: Colors.white,
+                        ),
+                  ),
                 ),
               ),
             ),
